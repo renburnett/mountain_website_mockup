6eec998061424a1649c2837fc13e74c5a79e5a31
renburnett <renardburnett@gmail.com>
2020-06-24 22:40:40 -0700

    great sstopping point!

--- index.js
+++ index.js
@@ -4,4 +4,32 @@ $(function() {
           scrollTop: ($($.attr(this, "href")).offset).top 
         }, 500)
   });
+
+  $(".slider").slick({
+    dots: true,
+    autoplay: true,
+    arrows: false,
+    infinite: true,
+    speed: 200,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    responsive: [
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+
 });
+
